e9969543169225c5c66baf62ba486804bb0ed245
Hieu Pham <hieu02617@gmail.com>
2025-06-12 23:26:21 +0700

    create new user from reactjs

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,29 +2,76 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManage.scss";
-import { getAllUsers } from "../../services/userService";
+import { getAllUsers, createNewUserService } from "../../services/userService";
+import ModalUser from "./ModalUser";
 class UserManage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrUsers: [],
+      isOpenModalUser: false,
     };
   }
 
   async componentDidMount() {
+    await this.getAllUserFromReact();
+  }
+
+  getAllUserFromReact = async () => {
     let response = await getAllUsers("ALL");
     if (response && response.errCode === 0) {
       this.setState({
         arrUsers: response.users,
       });
     }
-  }
+  };
+
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser: true,
+    });
+  };
+
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModalUser: !this.state.isOpenModalUser,
+    });
+  };
+
+  createNewUser = async (data) => {
+    try {
+      let response = await createNewUserService(data);
+      if (response && response.errCode !== 0) {
+        alert(response.errMessage);
+      } else {
+        await this.getAllUserFromReact();
+        this.setState({
+          isOpenModalUser: false,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   render() {
     let arrUsers = this.state.arrUsers;
     return (
       <div className="users-container">
+        <ModalUser
+          isOpen={this.state.isOpenModalUser}
+          toggleFromParent={this.toggleUserModal}
+          createNewUser={this.createNewUser}
+        />
         <div className="title text-center">Manage users with PVH</div>
+        <div className="mx-1">
+          <button
+            className="btn btn-primary px-3"
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i class="fas fa-plus"></i> Add new users
+          </button>
+        </div>
         <div className="users-table mt-4 mx-3">
           <table id="customers">
             <tr>
@@ -36,7 +83,6 @@ class UserManage extends Component {
             </tr>
             {arrUsers &&
               arrUsers.map((item, index) => {
-                console.log("PVH check map: ", item, index);
                 return (
                   <tr>
                     <td>{item.email}</td>
